a528ffd2f9bbe75ba5b4a0ffb0e216ba2e59c69a
Alberto Rico <alrico88@gmail.com>
2024-08-26 01:33:39 +0200

    fix: open credits links in new tab

--- src/lib/Credits.svelte
+++ src/lib/Credits.svelte
@@ -1,8 +1,11 @@
 <ul class="list-unstyled mb-0">
-	<li>Made by <a href="https://alrico.es">Alberto Rico</a></li>
+	<li>Made by <a href="https://alrico.es" target="_blank">Alberto Rico</a></li>
 	<li>
-		Uses <a href="https://www.npmjs.com/package/@tmcw/togeojson">@tmcw/togeojson</a> under the hood
+		Uses <a href="https://www.npmjs.com/package/@tmcw/togeojson" target="_blank">@tmcw/togeojson</a>
+		under the hood
 	</li>
-	<li>Code on <a href="https://github.com/alrico88/kml-to-geojson">Github</a></li>
-	<li>Found it useful? <a href="https://buymeacoffee.com/albertorico">Support me</a></li>
+	<li>Code on <a href="https://github.com/alrico88/kml-to-geojson" target="_blank">Github</a></li>
+	<li>
+		Found it useful? <a href="https://buymeacoffee.com/albertorico" target="_blank">Support me</a>
+	</li>
 </ul>
